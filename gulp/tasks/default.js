1078f188e494ab7013db99198f45cef28b9ac3f4
diogorusso <hello@diogorusso.com>
2016-09-10 15:10:24 -0700

    Update 2016-09-10T22:10:21.935Z

--- gulp/tasks/default.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-var runSequence = require('gulp-run-sequence');
-
-gulp.task('default', function(cb) {
-  runSequence('webpack', 'jekyll-dev','watch', cb);
-});
--- gulp/tasks/watch.js
+++
@@ -1,7 +0,0 @@
-var gulp   = require('gulp');
-var config = require('../config').watch;
-
-gulp.task('watch', ['browsersync'], function() {
-  gulp.watch(config.jekyll,['jekyll-rebuild']);
-  gulp.watch(config.webpack,['webpack']);
-});
